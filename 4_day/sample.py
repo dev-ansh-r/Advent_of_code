5102d84178a001225fa40a133a92ec7b64aab0ae
dev-ansh-r <rdevanshshukla26@gmail.com>
2022-12-04 15:41:58 +0530

    sample and another logic

--- 4_day/sample.py
+++ 4_day/sample.py
@@ -1,9 +1,15 @@
 f=open("sample.txt","r")
-indexrange = lambda a, b: range(a, b + 1)
-count=0
 
-for line in f:
-    a, b = line.strip().split(",")
-    a = set(indexrange(*map(int, a.split("-"))))
-    b = set(indexrange(*map(int, b.split("-"))))
-    print(a&b)
+def p1(f):
+    count=0
+    for line in f:
+        a,b=line.strip().split(",")
+        a0,a1=a.split('-')
+        b0,b1=b.split('-')
+        if int(a0)<=int(b0) and int(a1)>=int(b1):
+            count+=1
+        elif int(a0)>=int(b0) and int(a1)<=int(b1):
+            count+=1
+    return count
+
+print(p1(f))
