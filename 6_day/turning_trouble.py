20f3898829e22ed02bacfb69a94dcf7ae1dc539d
dev-ansh-r <rdevanshshukla26@gmail.com>
2022-12-06 11:52:56 +0530

    day 6

---
+++ 6_day/turning_trouble.py
@@ -0,0 +1,36 @@
+f=open("input.txt","r")
+
+def uniqueCharacters(str):
+     
+    # If at any time we encounter 2
+    # same characters, return false
+    for i in range(len(str)):
+        for j in range(i + 1,len(str)):
+            if(str[i] == str[j]):
+                return False;
+    return True;
+
+def p1(f):
+    for l in f:
+        count=[]
+        j=0
+        for i in range(4,len(l)):
+            Mystr=l[j:i]
+            j+=1
+            if(uniqueCharacters(Mystr)):
+                count.append(i)
+    return count[0]
+
+def p2(f):
+    for l in f:
+        count=[]
+        j=0
+        for i in range(14,len(l)):
+            Mystr=l[j:i]
+            j+=1
+            if(uniqueCharacters(Mystr)):
+                count.append(i)
+    return count[0]
+
+#print(p1(f))
+print(p2(f))
